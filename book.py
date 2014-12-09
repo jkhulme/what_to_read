7a880a7e22fda32878e621149b0f716f0e91d55f
James Hulme <github@jkhulme.fastmail.fm>
2014-12-09 15:01:09 +0100

    can get random book and display its info

--- book.py
+++ book.py
@@ -2,14 +2,19 @@ class Book(object):
 
     def __init__(self, book_data):
         self.title = book_data.get("name", "ERR: Missing Title")
-        self.authors = self.author_names(book_data.get("authors", ["ERR: Missing Authors"]))
+        self.authors = self.author_names(book_data.get("authors", [{}]))
         self.description = book_data.get("description", "ERR: Missing description")
         self.price = book_data.get("price", "ERR: Missing Price")
         self.currency = book_data.get("price_currency", "ERR: Missing Currency")
         self.sample_url = book_data.get("sample_url", "ERR: Missing Sample URL")
         self.uri = book_data.get("resource_uri", "ERR: Missing URI")
-        self.image = book_data.get("main_image", "ERR: Missing Image")
+        self.image = self.get_image_url(book_data.get("main_image", [{}]))
 
     def author_names(self, authors):
+        author_names = []
         for author in authors:
-            print author
+            author_names.append(author.get("name", "Author Missing"))
+        return author_names
+
+    def get_image_url(self, image):
+        return image.get("image", {}).get("640x640", "ERR:Missing image")
